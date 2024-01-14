684f21b0c960bab15ee36a37d99aab625585bed9
axelobrikat <axel.obrikat@arcor.de>
2024-01-14 17:37:05 +0100

    test_exception_handling

---
+++ test/src/utils/test_exception_handling.py
@@ -0,0 +1,41 @@
+import pytest
+from pytest import LogCaptureFixture
+
+from src.utils import exception_handling as exc
+
+def test_clear_catched_exceptions():
+    exc.EXC.append("test")
+    exc.clear_catched_exceptions()
+    assert len(exc.EXC) == 0
+
+def test_log_exc(caplog: LogCaptureFixture):
+    exc_msg = "Test Exception"
+    err_msg = "Test Value Error"
+    exc_info = (ValueError, ValueError(err_msg), None)
+    exc.log_exc(exc_msg, exc_info)
+    assert f"{exc_msg}" in caplog.text
+    assert f"{err_msg}" in caplog.text
+
+def test_process_exc(caplog: LogCaptureFixture):
+    exc.clear_catched_exceptions()
+    exc_msg = "Test Exception"
+    with pytest.raises(Exception):
+        exc.raise_exception(exc_msg)
+    assert len(exc.EXC) == 1
+    assert f"{exc_msg}" in caplog.text
+
+def test_program_end_without_exceptions(caplog: LogCaptureFixture):
+    exc.clear_catched_exceptions()
+    exc.program_end()
+    assert caplog.text.strip().endswith("Program ends..")
+    
+def test_program_end_with_exception(caplog: pytest.LogCaptureFixture):
+    exc.clear_catched_exceptions()
+    exc_msg = "Test Exception"
+    err_msg = "Test Error"
+    exc_info = (ValueError, ValueError(err_msg), None)
+    exc.EXC.append([exc_msg, exc_info])
+    exc.program_end()
+    assert "Program ends.." in caplog.text
+    assert "Roundup of catched exceptions" in caplog.text
+    assert f"{exc_msg}" in caplog.text
